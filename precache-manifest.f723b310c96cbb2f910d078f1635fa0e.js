d624e2c9c9c2afb0916220865ff829ef4bcfcee0
julesntare <julesntare@gmail.com>
2022-05-20 16:09:08 +0200

    Updates

---
+++ precache-manifest.f723b310c96cbb2f910d078f1635fa0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "60982d9206050d2b833a",
+    "url": "/zavensy/static/css/main.abdc0b46.chunk.css"
+  },
+  {
+    "revision": "60982d9206050d2b833a",
+    "url": "/zavensy/static/js/main.60982d92.chunk.js"
+  },
+  {
+    "revision": "cb5712f862845c5a12f9",
+    "url": "/zavensy/static/js/1.cb5712f8.chunk.js"
+  },
+  {
+    "revision": "a7c5bffeff8abcb50e4a",
+    "url": "/zavensy/static/js/runtime~main.a7c5bffe.js"
+  },
+  {
+    "revision": "d8dd0295d28140a4d514b23d0e5a4a1b",
+    "url": "/zavensy/index.html"
+  }
+];
